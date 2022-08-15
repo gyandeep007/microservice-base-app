a6092e31d06951c32809a2ca4d8ac543e07dcfe1
gyandeep <gyandeepsharma007@gmail.com>
2022-08-15 18:22:35 +0530

    base-app

---
+++ src/test/java/com/gyan/microservicebaseapp/MicroserviceBaseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyan.microservicebaseapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceBaseAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

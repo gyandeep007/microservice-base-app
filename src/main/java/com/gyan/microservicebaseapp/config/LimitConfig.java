058dc8737a7f6804d3e06d4ad0c3f0a4ad2ef8cb
gyandeep <gyandeepsharma007@gmail.com>
2022-08-16 11:38:20 +0530

    add code for returning limit via spring cloud config server

--- src/main/java/com/gyan/microservicebaseapp/config/LimitConfig.java
+++ src/main/java/com/gyan/microservicebaseapp/config/LimitConfig.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @NoArgsConstructor
